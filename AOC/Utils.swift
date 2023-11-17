585acc14513b9d41ec6f95d90a83a39f857b1c37
Anshul Kanwar <me@anshulkanwar.com>
2023-11-17 16:04:51 +0530

    add `Puzzle` Protocol

--- AOC/Utils.swift
+++ AOC/Utils.swift
@@ -7,6 +7,21 @@
 
 import Foundation
 
+protocol Puzzle {
+    associatedtype Input: Any
+    var input: Input { get }
+    
+    init(input: Input)
+    init(inputStr: String)
+    
+    func part1() -> Any
+    func part2() -> Any
+}
+
+func readInput(forDay dayNumber: Int) -> String {
+    readFile("Inputs/day\(dayNumber).txt")
+}
+
 func readFile(_ filename: String) -> String {
     let cwd = FileManager.default.currentDirectoryPath
     let filepath = "\(cwd)/\(filename)"
