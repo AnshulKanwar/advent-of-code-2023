032c620bb5cfa78d60e455d1364f9166ad45ba6e
Anshul Kanwar <me@anshulkanwar.com>
2023-12-01 22:26:24 +0530

    Day 1 Part 2

--- AOC/Puzzles/Day1.swift
+++ AOC/Puzzles/Day1.swift
@@ -5,6 +5,9 @@
 //  Created by Anshul Kanwar on 01/12/23.
 //
 
+import Foundation
+import RegexBuilder
+
 final class Day1: Puzzle {
     var input: [String]
     
@@ -27,6 +30,24 @@ final class Day1: Puzzle {
     }
     
     func part2() -> Any {
-        fatalError("Not Implemented")
+        fatalError("Could not implement")
+//        let numberFormatter = NumberFormatter()
+//        numberFormatter.numberStyle = .spellOut
+//       
+//        let calibrationMatcher = /(?=(one|two|three|four|five|six|seven|eight|nine))/
+//        
+//        return input.reduce(0) { sum, calibrationValue in
+//            let matches = calibrationValue.matches(of: calibrationMatcher).map { $0.output.1 }
+//            
+//            let firstDigitString = matches.first!
+//            let lastDigitString = matches.last!
+//            
+//            let firstDigit = numberFormatter.number(from: String(firstDigitString)) ?? Int(firstDigitString)! as NSNumber
+//            let lastDigit = numberFormatter.number(from: String(lastDigitString)) ?? Int(lastDigitString)! as NSNumber
+//            
+//            print("\(firstDigitString) \(lastDigitString) \t \(calibrationValue)")
+//            
+//            return sum + Int("\(firstDigit)\(lastDigit)")!
+//        }
     }
 }
--- AOC/main.swift
+++ AOC/main.swift
@@ -5,4 +5,6 @@
 //  Created by Anshul Kanwar on 17/11/23.
 //
 
-print("Hello, World!")
+let day1 = Day1(inputStr: readInput(forDay: 1))
+print(day1.part1())
+print(day1.part2())
