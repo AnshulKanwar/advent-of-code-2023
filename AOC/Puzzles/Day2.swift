34354576bbcc90d00406df25697c7c15f1ecaa10
Anshul Kanwar <me@anshulkanwar.com>
2023-12-02 18:09:46 +0530

    Day2 Part 1

---
+++ AOC/Puzzles/Day2.swift
@@ -0,0 +1,107 @@
+//
+//  Day2.swift
+//  AOC
+//
+//  Created by Anshul Kanwar on 02/12/23.
+//
+
+import RegexBuilder
+
+final class Day2: Puzzle {
+    typealias Input = [Substring: [Substring]]
+    
+    var input: [Substring: [Substring]]
+    
+    enum Cube: String {
+        case red, green, blue
+    }
+    
+    init(input: Input) {
+        self.input = input
+    }
+    
+    convenience init(inputStr: String) {
+        let games = inputStr
+            .split(separator: "\n")
+            .map {
+                $0
+                    .split(separator: ":")
+            }
+            .map {
+                ($0[0], $0[1])
+            }
+
+        let dict = Dictionary(uniqueKeysWithValues: games)
+            .mapValues {
+                $0.split(separator: ";")
+            }
+        
+        self.init(input: dict)
+    }
+    
+    func part1() -> Any {
+        let gameMatcher = Regex {
+            "Game"
+            CharacterClass.whitespace
+            Capture {
+                OneOrMore(.digit)
+            }
+        }
+        
+        let cubesMatcher = Regex {
+            Capture {
+                OneOrMore(.digit)
+            }
+            CharacterClass.whitespace
+            Capture {
+                ChoiceOf {
+                    "red"
+                    "green"
+                    "blue"
+                }
+            }
+        }
+        
+        var sum = 0
+        
+        for (game, result) in input {
+            let match = try! gameMatcher.firstMatch(in: game)!
+            let gameId = Int(match.1)!
+            
+            var isPossible = true
+            
+        outerloop: for cubes in result {
+                for match in cubes.matches(of: cubesMatcher) {
+                    let nCubes = Int(match.1)!
+                    let color = Cube(rawValue: String(match.2))!
+                    
+                    switch color {
+                    case .red:
+                        isPossible = nCubes <= 12
+                    case .green:
+                        isPossible = nCubes <= 13
+                    case .blue:
+                        isPossible = nCubes <= 14
+                    }
+                    
+                    if !isPossible {
+                        break outerloop
+                    }
+                }
+            }
+            
+            if isPossible {
+                print("Game \(gameId) is possible")
+                sum += gameId
+            } else {
+                print("Game \(gameId) is not possible")
+            }
+        }
+        
+        return sum
+    }
+    
+    func part2() -> Any {
+        fatalError("Not Implemented")
+    }
+}
--- AOC/main.swift
+++ AOC/main.swift
@@ -5,6 +5,5 @@
 //  Created by Anshul Kanwar on 17/11/23.
 //
 
-let day1 = Day1(inputStr: readInput(forDay: 1))
-print(day1.part1())
-print(day1.part2())
+let day2 = Day2(inputStr: readInput(forDay: 2))
+print(day2.part1())
